6d4a04d227a31df66c3c07ab29300a7ed0309614
cippero <gilwein@gmail.com>
2018-05-08 13:21:12 -0700

    merging original app.js with oauth file

--- serverTEMP.js
+++ serverTEMP.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
-var express = require('express');
-var bodyParser = require('body-parser');
-var logger = require('express-logger');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var inspect = require('util-inspect');
-var oauth = require('oauth');
-// var MemoryStore = require('memorystore')(session); //might cause issues after deploying
-
+var express       = require('express');
+var bodyParser    = require('body-parser');
+var logger        = require('express-logger');
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
+var inspect       = require('util-inspect');
+var oauth         = require('oauth');
+var morgan        = require('morgan');
+var createError   = require('http-errors');
+var indexRouter   = require('./routes/index');
+var usersRouter   = require('./routes/users');
+var authRouter    = require('./routes/auth');
+var tweetRouter   = require('./routes/tweets');
+var path          = require('path');
 var app = express();
 
 var consumer = new oauth.OAuth(
@@ -23,11 +28,39 @@ app.use(session({
 	resave: true, 
 	saveUninitialized: true
 }));
-
+app.use(morgan('dev'));
 app.use(function(req, res, next) {
   res.locals.session = req.session;
   next();
 });
+
+
+///////////////////////////////////////
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+app.use('/tweets', tweetRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+//////////////////////////////////////////
 
 
 app.get('/sessions/connect', function(req, res){
@@ -37,11 +70,10 @@ app.get('/sessions/connect', function(req, res){
     } else {  
       req.session.oauthRequestToken = oauthToken;
       req.session.oauthRequestTokenSecret = oauthTokenSecret;
-      console.log("Double check on 2nd step");
-      console.log("------------------------");
-      console.log("<<"+req.session.oauthRequestToken);
-      console.log("<<"+req.session.oauthRequestTokenSecret);
-      //console.log("##############\n",req.session)
+      // console.log("Double check on 2nd step");
+      // console.log("------------------------");
+      // console.log("<<"+req.session.oauthRequestToken);
+      // console.log("<<"+req.session.oauthRequestTokenSecret);
       res.redirect("https://twitter.com/oauth/authorize?oauth_token="+req.session.oauthRequestToken);      
     }
   });
@@ -49,34 +81,25 @@ app.get('/sessions/connect', function(req, res){
 
 
 app.get('/sessions/callback', function(req, res){
-  console.log("##############\n",req.session)
-  console.log("------------------------");
-  console.log(">>"+req.session.oauthRequestToken);
-  console.log(">>"+req.session.oauthRequestTokenSecret);
-  console.log(">>BODY", req.body);
-  console.log(">>QUERY", req.query);
-  console.log(">>"+req.query.oauth_verifier);
+  // console.log("------------------------");
+  // console.log(">>"+req.session.oauthRequestToken);
+  // console.log(">>"+req.session.oauthRequestTokenSecret);
+  // console.log(">>"+req.query.oauth_verifier);
   consumer.getOAuthAccessToken(req.query.oauth_token, '', req.query.oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
     if (error) {
       res.status(500).send("Error getting OAuth access token : " + inspect(error) + "[" + oauthAccessToken + "]" + "[" + oauthAccessTokenSecret + "]" + "[" + inspect(results) + "]");
     } else {
-      console.log('no error!');
-      console.log(oauthAccessToken, oauthAccessTokenSecret);
-      console.log('session is now', req.session);
       req.session.oauthAccessToken = oauthAccessToken;
       req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
-      console.log('session is now', req.session);
-      
       res.redirect('/home');
     }
   });
 });
 
 app.get('/home', function(req, res){
-    // consumer.get("https://api.twitter.com/1.1/account/verify_credentials.json", req.session.oauthAccessToken, req.session.oauthAccessTokenSecret, function (error, data, response) {
     consumer.get("https://api.twitter.com/1.1/account/verify_credentials.json", process.env.ACCESS_TOKEN, process.env.ACCESS_TOKEN_SECRET, function (error, data, response) {
       if (error) {
-        //console.log(error)
+        console.log(error)
         res.redirect('/sessions/connect');
       } else {
         var parsedData = JSON.parse(data);
@@ -92,3 +115,9 @@ app.get('*', function(req, res){
 app.listen(8080, function() {
   console.log('App runining on port 8080!');
 });
+
+////////////////////////////////////////////////////
+var http = require('http');
+module.exports = app;
+var server = http.createServer(app);
+server.listen(4007);
