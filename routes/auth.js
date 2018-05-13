771dac54b0a0bbc52541cb8a513e625aa5a5141e
thegreatsantini <lss13@my.fsu.edu>
2018-05-13 15:05:30 -0700

    edit seed.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var passport = require('passport');
 var router = express.Router();
-
+const db = require('../models')
 // This route checks for the existence of a user in the session
 router.get('/user', (req, res, next) => {
-	let userName = '';
+	// let userName = '';
+	db.users.find({'displayName': 'name2'}, function(req, res) {
+		res.json(res)
+	})
 	// console.log(req.user);
 	// if (Object.keys(req.sessionStore.sessions).length === 0 && req.sessionStore.sessions.constructor === Object) {
 	// 	let key = Object.keys(req.sessionStore.sessions)[0];
